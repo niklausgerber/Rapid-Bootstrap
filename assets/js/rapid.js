54370449bc4495e9014b4ea3341692e896edb2fe
Niklaus Gerber <niklausgerber@shortmail.com>
2013-03-21 09:31:30 +0100

    Some performance issues have been resolved.

---
+++ assets/js/rapid.js
@@ -0,0 +1,10 @@
+/* jQuery Plugin */
+/* @include _jquery-1.9.0.min */
+
+/* CSS3 Media Queries */
+/* @include _respond.min */
+
+/* retina.js (provide image alternative | my_image@2x.png) */
+/* @include _retina.js */
+
+/* @include _scripts.js */
